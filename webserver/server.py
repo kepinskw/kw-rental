63a9807261442dbf6f9bf56306fe3088e796a5bf
wk <wiktorkx5@gmail.com>
2024-04-12 17:38:36 +0200

    feat: added contact form actions

--- webserver/server.py
+++ webserver/server.py
@@ -1,43 +1,38 @@
-from flask import Flask, render_template
+import csv
+
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, static_folder='static', static_url_path='/')
 
 
 @app.route("/")
-def hello_world():
+def home_page():
     return render_template("index.html")
 
 
-@app.route("/index.html")
-def home():
-    return render_template("index.html")
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route("/about.html")
-def about():
-    return render_template("about.html")
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, subject, message])
 
 
-@app.route("/services.html")
-def services():
-    return render_template("services.html")
-
-
-@app.route("/loc.html")
-def loc():
-    return render_template("loc.html")
-
-
-@app.route("/rooms.html")
-def rooms():
-    return render_template("rooms.html")
-
-
-@app.route("/studio.html")
-def studio():
-    return render_template("studio.html")
-
-
-@app.route("/contact.html")
-def contact():
-    return render_template("contact.html")
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thanku.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong. Please try again.'
